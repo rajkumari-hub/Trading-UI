093b4f0c945f8ed2014402a26a4f1e564212c7f2
root <root@ip-172-31-38-52.ec2.internal>
2026-09-09 07:26:02 +0000

    Mock Axios requests in Purchase test

--- src/__tests__/Purchase/Purchase.test.js
+++ src/__tests__/Purchase/Purchase.test.js
@@ -1,26 +1,54 @@
-import React,{Component} from 'react';
-import { shallow } from 'enzyme';import Purchase from '../../Components/Purchase/Purchase';
-describe('when the home component is called',()=>{
+import React from 'react';
+import { shallow } from 'enzyme';
+import axios from 'axios';
+import Purchase from '../../Components/Purchase/Purchase';
+
+jest.mock('axios');
+
+describe('when the home component is called', () => {
     let wrapper;
-    beforeEach(()=>{
-        wrapper=shallow(<Purchase />);
+
+    beforeEach(() => {
+        axios.get.mockResolvedValue({
+            status: 200,
+            data: {
+                status: 'SUCCESS',
+                data: []
+            }
+        });
+
+        axios.post.mockResolvedValue({
+            status: 200,
+            data: {
+                status: 'SUCCESS',
+                totalPrice: 100
+            }
+        });
+
+        wrapper = shallow(<Purchase />);
     });
-    it('should render the render method',()=>{
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should render the render method', () => {
         expect(wrapper).toHaveLength(1);
     });
-    it('should render the 1 button',()=>{
+
+    it('should render the 1 button', () => {
         expect(wrapper.find('button')).toHaveLength(2);
     });
+
     it('should have called handle click book function', () => {
         const spy = jest.spyOn(wrapper.instance(), 'handleSubmit');
-        wrapper.instance().forceUpdate();
-        wrapper.find('#submit').simulate('click',{
-          preventDefault: () => {
-          }
-         });
-        expect(spy).toHaveBeenCalled();
-      });
-    
-   
-})
 
+        wrapper.instance().forceUpdate();
+
+        wrapper.find('#submit').simulate('click', {
+            preventDefault: () => {}
+        });
+
+        expect(spy).toHaveBeenCalled();
+    });
+});
