b877c1de35810081cae60a1b38328c04ec6d3f87
root <root@ip-172-31-38-52.ec2.internal>
2026-09-09 07:13:33 +0000

    Fix Jest test environment compatibility

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,19 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+const React = require('react');
+const PropTypes = require('prop-types');
+const { TextEncoder, TextDecoder } = require('util');
+const { ReadableStream } = require('stream/web');
+const { MessageChannel, MessagePort } = require('worker_threads');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+global.ReadableStream = ReadableStream;
+global.MessageChannel = MessageChannel;
+global.MessagePort = MessagePort;
+
+// Compatibility for old libraries such as sweetalert-react
+React.PropTypes = PropTypes;
+
+const { configure } = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
 configure({ adapter: new Adapter() });
